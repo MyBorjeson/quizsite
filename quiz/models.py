a9c18c5f445e9c3626f6443d0456c64cc9885ee3
My Boörjeson <myborjeson@gmail.com>
2015-08-06 23:16:50 +0200

    Unicode for smoother Django admin UI

--- quiz/models.py
+++ quiz/models.py
@@ -5,8 +5,8 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
-	# def __unicode__(self):
-	# 	return self.name
+	def __unicode__(self):
+		return self.name
 
 class Question(models.Model):
 	quiz = models.ForeignKey(Quiz,related_name="questions")
@@ -15,5 +15,5 @@ class Question(models.Model):
 	answer2 = models.CharField(max_length=100)
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
-	# def __unicode__(self):
-	# 	return self.name+"/"+self.question
+	def __unicode__(self):
+		return self.quiz.name + " / " + self.question
