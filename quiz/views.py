c461f0363c998e7fc97e490684fa8c2e9b9303d0
My Boörjeson <myborjeson@gmail.com>
2015-08-06 10:11:57 +0200

    Added my views into the views.py file
    Start page, quiz start page, question one and two page.

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+return render(request, "quiz/templates/startpage.html")
+def quiz(request):
+return render(request, "quiz/templates/start-quiz.html")
+def questionone(request):
+return render(request, "quiz/templates/questionone.html")
+def questiontwo(request):
+return render(request, "quiz/templates/questiontwo.html")
+def result(request):
+return render(request, "quiz/templates/resultpage.html")
