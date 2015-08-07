96d8a8add306ad093072ab378db6759d8a10f1ad
My Boörjeson <myborjeson@gmail.com>
2015-08-07 16:36:42 +0200

    Revert "Try changes - debugging"
    This reverts commit 44cdc74b712c37d31ad60740f0a84fe7abd7ee9c.

--- quizsite/wsgi.py
+++ quizsite/wsgi.py
@@ -4,4 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizsite.settings")
 
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
+
